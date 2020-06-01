a2f347cbd1aef9c8756a3b05bd4c794e5cf23008
ana giron <agiro011@gmail.com>
2020-06-01 21:53:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string.toUpperCase()){
     return "YES INDEED"};
-} else if (string.toLowerCase()){
+else if (string.toLowerCase()){
     return "I can't hear you!"};
 } else if (string === 'I love you, Grandma'){
   return 'I love you, too'};
